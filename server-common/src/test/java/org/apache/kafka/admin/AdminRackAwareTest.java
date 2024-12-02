524254e990a3e8beac688366edc85662a5d6cb90
chiacyu <yujuan476@gmail.com>
2024-12-02 19:22:11 +0800

    Updated

--- server-common/src/test/java/org/apache/kafka/admin/AdminRackAwareTest.java
+++ server-common/src/test/java/org/apache/kafka/admin/AdminRackAwareTest.java
@@ -40,6 +40,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AdminRackAwareTest {
+    public static class ReplicaDistributions {
+        private final Map<Integer, List<String>> partitionRacks;
+        private final Map<Integer, Integer> brokerLeaderCount;
+        private final Map<Integer, Integer> brokerReplicasCount;
+
+        public ReplicaDistributions(Map<Integer, List<String>> partitionRacks,
+                                    Map<Integer, Integer> brokerLeaderCount,
+                                    Map<Integer, Integer> brokerReplicasCount) {
+            this.partitionRacks = partitionRacks;
+            this.brokerLeaderCount = brokerLeaderCount;
+            this.brokerReplicasCount = brokerReplicasCount;
+        }
+
+        public Map<Integer, List<String>> getPartitionRacks() {
+            return partitionRacks;
+        }
+
+        public Map<Integer, Integer> getBrokerLeaderCount() {
+            return brokerLeaderCount;
+        }
+
+        public Map<Integer, Integer> getBrokerReplicasCount() {
+            return brokerReplicasCount;
+        }
+    }
 
     private static Collection<BrokerMetadata> toBrokerMetadata(Map<Integer, String> rackMap) {
         List<BrokerMetadata> brokerMetadatas = rackMap.entrySet().stream()
@@ -365,8 +390,8 @@ public class AdminRackAwareTest {
         for (List<Integer> replicas : assignment.values()) {
             assertEquals(replicationFactor, replicas.size());
         }
-        assignment.values().forEach(replicas-> {
-            assertEquals(replicationFactor, replicas.size());});
+        assignment.values().forEach(replicas -> {
+            assertEquals(replicationFactor, replicas.size()) ;});
 
         ReplicaDistributions distribution = getReplicaDistribution(assignment, brokerRackMapping);
 
